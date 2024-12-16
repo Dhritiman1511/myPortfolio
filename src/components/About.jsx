a144fb52f440197c2bf13aea957d72d098990a83
Dhritiman1511 <128951205+Dhritiman1511@users.noreply.github.com>
2024-12-16 23:07:39 +0530

    minor changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,37 +12,76 @@ function About() {
           />
           <div className="flex-1">
             <p className="text-lg mb-8">
-              I am a passionate full-stack developer with a focus on building
-              modern and responsive applications. With a strong foundation in
-              both frontend and backend technologies, I strive to create
-              seamless and efficient user experiences.
+              I&apos;m Dhritiman Roy, a passionate full-stack developer and data
+              analyst who loves crafting efficient digital solutions. With
+              expertise in building seamless user interfaces, scalable backend
+              systems, and mobile applications, I strive to deliver impactful
+              results. Beyond development, I enjoy mentoring aspiring
+              programmers and sharing my knowledge to help others grow.
             </p>
+
             <div className="space-y-4">
+              {/* HTML & CSS */}
               <div className="flex items-center">
-                <label htmlFor="htmlandcsss" className="w-2/12">
+                <label htmlFor="htmlcss" className="w-2/12">
                   HTML & CSS
                 </label>
                 <div className="grow bg-gray-800 rounded-full h-2.5">
                   <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-10/12"></div>
                 </div>
               </div>
+
+              {/* React.js */}
               <div className="flex items-center">
-                <label htmlFor="htmlandcsss" className="w-2/12">
-                  React JS
+                <label htmlFor="reactjs" className="w-2/12">
+                  React
                 </label>
                 <div className="grow bg-gray-800 rounded-full h-2.5">
-                  <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-11/12"></div>
+                  <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-9/12"></div>
                 </div>
               </div>
+
+              {/* Express.js */}
               <div className="flex items-center">
-                <label htmlFor="htmlandcsss" className="w-2/12">
-                  Python
+                <label htmlFor="expressjs" className="w-2/12">
+                  Express
                 </label>
                 <div className="grow bg-gray-800 rounded-full h-2.5">
                   <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-8/12"></div>
                 </div>
               </div>
+
+              {/* MongoDB */}
+              <div className="flex items-center">
+                <label htmlFor="mongodb" className="w-2/12">
+                  MongoDB
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-7/12"></div>
+                </div>
+              </div>
+
+              {/* Python */}
+              <div className="flex items-center">
+                <label htmlFor="python" className="w-2/12">
+                  Python
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-9/12"></div>
+                </div>
+              </div>
+
+              {/* Flutter */}
+              <div className="flex items-center">
+                <label htmlFor="flutter" className="w-2/12">
+                  Flutter
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full transform transition-transform duration-300 hover:scale-105 w-7/12"></div>
+                </div>
+              </div>
             </div>
+
             <div className="mt-12 flex justify-around text-center">
               <div>
                 <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
